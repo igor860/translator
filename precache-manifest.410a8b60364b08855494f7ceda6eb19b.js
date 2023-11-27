17e86feb9ae4a2ba4813243a2804cd374c3c89e0
igor860 <145049273+igor860@users.noreply.github.com>
2023-11-27 17:56:24 +0300

    Add files via upload

---
+++ precache-manifest.410a8b60364b08855494f7ceda6eb19b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24e2d0ebab6d298cff17405202bba7c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41edeaef128b783ba99c",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "fa40f4513e2e358d9c39",
+    "url": "/static/js/2.f6e95390.chunk.js"
+  },
+  {
+    "revision": "41edeaef128b783ba99c",
+    "url": "/static/js/main.fdfd8238.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
